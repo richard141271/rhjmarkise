3a0b90ebc4d5433addb17d2c432910e888437bd5
richard141271 <68450987+richard141271@users.noreply.github.com>
2025-12-04 02:55:16 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('rhj-v1').then(cache => cache.addAll(['/','/index.html','/assets/css/styles.css'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
 
